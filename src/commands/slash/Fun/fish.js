79ca663735251d5a858100ae93c9ce2c8ac290b0
Pascal Young <pascalyoung03@gmail.com>
2024-02-21 20:22:04 -0800

    fish again works

--- src/commands/slash/Fun/fish.js
+++ src/commands/slash/Fun/fish.js
@@ -1,6 +1,46 @@
-const {SlashCommandBuilder, EmbedBuilder, ButtonStyle, ActionRowBuilder, ButtonBuilder} = require('discord.js');
+const {SlashCommandBuilder, EmbedBuilder, ButtonStyle, ActionRowBuilder, ButtonBuilder, ComponentType} = require('discord.js');
 const {fish, generateXP} = require('../../../functions');
 const {User} = require('../../../schemas/UserSchema');
+
+const updateUserWithFish = async (userId) => {
+	const f = await fish('');
+	const user = await User.findOne({userId: userId});
+	if (user) {
+		user.inventory.fish.push(f);
+		user.stats.fishCaught++;
+		user.stats.latestFish = f;
+		user.stats.soldLatestFish = false;
+		user.xp += generateXP();
+		user.save();
+	};
+
+	return f;
+}
+
+const followUpMessage = async (interaction, user, f) => {
+	return await interaction.followUp({
+		embeds: [
+			new EmbedBuilder()
+				.setTitle('Fished!')
+				.addFields(
+					{name: 'Congratulations!', value: `<${f.icon?.animated ? 'a' : ''}:${f.icon.data}> ${user.globalName} caught **${f.rarity}** ${f.name}!`},
+				),
+		],
+		components: [
+			new ActionRowBuilder()
+				.addComponents(
+					new ButtonBuilder()
+						.setCustomId('fish-again')
+						.setLabel('Fish again!')
+						.setStyle(ButtonStyle.Primary),
+					new ButtonBuilder()
+						.setCustomId('sell-one-fish')
+						.setLabel('Sell')
+						.setStyle(ButtonStyle.Danger),
+				),
+		],
+	});
+}
 
 module.exports = {
 	customId: 'fish-again',
@@ -14,41 +54,28 @@ module.exports = {
      * @param {ExtendedClient} client
      * @param {ChatInputCommandInteraction} interaction
      */
-	async run(client, interaction) {
+	async run(client, interaction, user=null) {
+		if (user === null) user = interaction.user
+
 		await interaction.deferReply();
 
-		const f = await fish('');
-		const user = await User.findOne({userId: interaction.user.id});
-		if (user) {
-			user.inventory.fish.push(f);
-			user.stats.fishCaught++;
-			user.stats.latestFish = f;
-			user.stats.soldLatestFish = false;
-			user.xp += generateXP();
-			user.save();
-		}
+		const newFish = await updateUserWithFish(user.id)
 
-		await interaction.followUp({
-			embeds: [
-				new EmbedBuilder()
-					.setTitle('Fished!')
-					.addFields(
-						{name: 'Congratulations!', value: `<${f.icon?.animated ? 'a' : ''}:${f.icon.data}> You caught **${f.rarity}** ${f.name}!`},
-					),
-			],
-			components: [
-				new ActionRowBuilder()
-					.addComponents(
-						new ButtonBuilder()
-							.setCustomId('fish-again')
-							.setLabel('Fish again!')
-							.setStyle(ButtonStyle.Primary),
-						new ButtonBuilder()
-							.setCustomId('sell-one-fish')
-							.setLabel('Sell')
-							.setStyle(ButtonStyle.Danger),
-					),
-			],
+		const followUp = await followUpMessage(interaction, user, newFish)
+
+		//const filter = () => interaction.user.id === interaction.message.author.id;
+
+		const collector = followUp.createMessageComponentCollector({
+			componentType: ComponentType.Button,
+			//filter,
+			time: 10000,
 		});
+
+		collector.on('collect', async collectionInteraction => {
+			if (collectionInteraction.user.id !== user.id) return;
+			if (collectionInteraction.customId === 'fish-again') {
+				await this.run(client, collectionInteraction, user)
+			}
+		})
 	},
 };
