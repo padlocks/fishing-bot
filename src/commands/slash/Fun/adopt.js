4ed46c2b33278d9780ceee6ae24d922f9c9d87fb
Pascal Young <pascalyoung03@gmail.com>
2024-05-31 08:29:10 -0700

    feature: adoption system

--- src/commands/slash/Fun/adopt.js
+++ src/commands/slash/Fun/adopt.js
@@ -2,8 +2,10 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { getUser } = require('../../../util/User');
 const { Pet } = require('../../../class/Pet');
 const { FishData, Fish } = require('../../../schemas/FishSchema');
-const { Aquarium } = require('../../../schemas/HabitatSchema');
+const { Habitat } = require('../../../schemas/HabitatSchema');
+const { Aquarium } = require('../../../class/Aquarium');
 const { capitalizeWords } = require('../../../util/Utils');
+const { PetFish } = require('../../../schemas/PetSchema');
 
 module.exports = {
 	structure: new SlashCommandBuilder()
@@ -41,42 +43,43 @@ module.exports = {
 		species = capitalizeWords(species.toLowerCase());
 		// check if species exists in database
 		const exists = await Fish.exists({ name: species });
-		if (!exists) return interaction.followUp({ content: 'That species does not exist!', ephemeral: true });
+		if (!exists) return interaction.editReply({ content: 'That species does not exist!', ephemeral: true });
+
 		const name = interaction.options.getString('name');
+		// check if name is already in use
+		const nameExists = await PetFish.exists({ name: name, owner: interaction.user.id });
+		if (nameExists) return interaction.editReply({ content: 'That name is already in use!', ephemeral: true });
+
 		const aquariumName = interaction.options.getString('aquarium');
+		// check if aquarium exists in database
+		const aquariumExists = await Habitat.exists({ name: aquariumName, owner: interaction.user.id });
+		if (!aquariumExists) return interaction.editReply({ content: 'That aquarium does not exist! Use the `build` command to construct one.', ephemeral: true });
 
 		const user = await getUser(interaction.user.id);
-		if (!user) return interaction.followUp({ content: 'There was an error retrieving your data!', ephemeral: true });
+		if (!user) return interaction.editReply({ content: 'There was an error retrieving your data!', ephemeral: true });
 
-		// check if user has an aquarium
-		const aquariums = await user.inventory.aquariums;
-		if (!aquariums.length) {
-			return interaction.followUp({
-				embeds: [
-					new EmbedBuilder()
-						.setTitle('Adoption Agency')
-						.addFields(
-							{ name: 'Uh-oh..', value: 'You do not have an aquarium to adopt a fish!' },
-						),
-				],
-			});
-		}
-
-		const aquariumData = await user.inventory.aquariums.find((a) => a.name.toLowerCase() === aquariumName.toLowerCase());
+		// find the desired aquarium
+		const aquariums = await Promise.all(user.inventory.aquariums.map(async (a) => await Habitat.findById(a)));
+		const aquariumData = await aquariums.find((a) => a.name.toLowerCase() === aquariumName.toLowerCase());
 		const aquarium = new Aquarium(aquariumData);
 
-		const fishInInventory = await user.inventory.fish.find((f) => f.name.toLowerCase() === species.toLowerCase() && !f.locked);
-		const fish = await FishData.findById(fishInInventory.id);
+		// find the desired fish in user's inventory
+		const fishes = await Promise.all(user.inventory.fish.map(async (f) => await FishData.findById(f)));
+		const fishInInventory = await fishes.find((f) => f.name.toLowerCase() === species.toLowerCase() && !f.locked);
+
+		// remove fish from inventory
+		user.inventory.fish = user.inventory.fish.filter((f) => !f.equals(fishInInventory.id));
+		await user.save();
 
 		let success = false;
 		let newPet;
-		if (fish) {
+		if (fishInInventory) {
 			newPet = new Pet({
-				fish: fish.id,
-				aquarium: aquarium.getId(),
+				fish: fishInInventory.id,
+				aquarium: await aquarium.getId(),
 				name: name,
 				age: 1,
-				owner: user._id,
+				owner: user.userId,
 				traits: [],
 				health: 100,
 				mood: 100,
@@ -87,8 +90,11 @@ module.exports = {
 
 			success = await newPet.save();
 			if (success) {
-				await aquarium.addFish(newPet.getId());
+				await aquarium.addFish(await newPet.getId());
 			}
+		}
+		else {
+			return interaction.editReply({ content: 'You do not have that fish in your inventory! Make sure it is unlocked.', ephemeral: true });
 		}
 
 		if (success) {
@@ -97,7 +103,7 @@ module.exports = {
 					new EmbedBuilder()
 						.setTitle('Adoption Agency')
 						.addFields(
-							{ name: 'Congratulations!', value: `You have successfully adopted the ${species} **${name}**! They have been added to ${aquariumName}.` },
+							{ name: 'Congratulations!', value: `You have successfully adopted the ${species} **${name}**! They have been added to aquarium **${aquariumName}**.` },
 						),
 				],
 			});
