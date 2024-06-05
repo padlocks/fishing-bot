57a68481578083f8636cc7500c3c1ec4eebd8f1e
Pascal Young <pascalyoung03@gmail.com>
2024-06-05 08:45:06 -0700

    add gacha opened stat

--- src/class/User.js
+++ src/class/User.js
@@ -323,6 +323,10 @@ class User {
 			box.count--;
 			await box.save();
 
+			const stats = await this.getStats();
+			stats.gachaBoxesOpened++;
+			await this.setStats(stats);
+
 			// remove box from user inventory
 			if (box.count <= 0) {
 				user.inventory.gacha = user.inventory.gacha.filter((i) => i.valueOf() !== box.id);
